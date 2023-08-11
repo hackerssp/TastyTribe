a9ca6b8d8d128a3194932e3294bad88be6765240
subhojeet chowdhury <subhojeetchowdhury98@gmail.com>
2023-08-11 13:46:13 +0530

    Data for blog page

---
+++ src/data/recipeTipsAndTricks.ts
@@ -0,0 +1,53 @@
+import marinades from "../assets/blog_images/marinades.jpg";
+import herbs from "../assets/blog_images/herbs.jpg";
+import poachedEggs from "../assets/blog_images/poached_eggs.jpg";
+import almondMilk from "../assets/blog_images/almond_milk.jpg";
+import baking from "../assets/blog_images/baking.jpg";
+import meat from "../assets/blog_images/meat.jpg";
+
+const recipeTipsAndTricks = [
+  {
+    title: "Flavorful Marinades",
+    description:
+      "Elevate your meats with marinades. Combine herbs, spices, acids like citrus, and oils to create tantalizing flavors. Let meats soak for a few hours or overnight for maximum taste infusion.",
+    tags: ["Tips and Tricks"],
+    image: marinades,
+  },
+  {
+    title: "Fresh Herb Brilliance",
+    description:
+      "Fresh herbs add a burst of vibrancy. To retain their flavor, add delicate herbs like basil after cooking, but toss heartier ones like rosemary and thyme in early for deeper infusion.",
+    tags: ["Tips and Tricks"],
+    image: herbs,
+  },
+  {
+    title: "Perfectly Poached Eggs",
+    description:
+      "For silky poached eggs, add a splash of vinegar to simmering water. Create a gentle whirlpool before cracking the egg into the center, aiding in the perfect shape.",
+    tags: ["Tips and Tricks"],
+    image: poachedEggs,
+  },
+  {
+    title: "Creamy Dairy Substitutes",
+    description:
+      "Replace heavy cream with healthier alternatives. Blend soaked cashews for a creamy base in sauces, or use coconut milk for luscious curries and soups without sacrificing flavor.",
+    tags: ["Tips and Tricks"],
+    image: almondMilk,
+  },
+  {
+    title: "Precise Baking Measurements",
+    description:
+      "For accurate baking, weigh ingredients with a kitchen scale. This guarantees consistent results as cup measurements can vary due to factors like ingredient density and packing.",
+    tags: ["Tips and Tricks"],
+    image: baking,
+  },
+  {
+    title: "Optimal Meat Resting",
+    description:
+      "Allow cooked meats to rest before slicing. Resting lets the juices redistribute, leading to juicier results. Cover with foil and rest for about 10 minutes for best results.",
+    tags: ["Tips and Tricks"],
+    image: meat,
+  },
+];
+
+export default recipeTipsAndTricks;
